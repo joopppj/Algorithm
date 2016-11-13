1da9abc10dd059b89966f5c0932d6186962429d3
Zhousenye Liu <jooppp@live.com>
2016-11-12 23:18:34 -0500

    Create reverseString.java

---
+++ String/reverseString.java
@@ -0,0 +1,15 @@
+public class reverseString{
+public static String reverseString(String s) {
+        char[] word = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(word);
+    }
+  }
