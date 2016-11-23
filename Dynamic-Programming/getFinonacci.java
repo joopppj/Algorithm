afad28e22b956183933e5348906f50a63d2903ae
Zhousenye Liu <jooppp@live.com>
2016-11-23 17:01:16 -0500

    add another approach

--- Dynamic-Programming/getFinonacci.java
+++ Dynamic-Programming/getFinonacci.java
@@ -11,4 +11,18 @@ public class getFinonacci{
     }
     // this is classical dynamic programming example from cc189
     //  if we use recursion directly , we will make many duplicates, which consume lots of time. So, we need to use an array to memorize those sub questions we already solved. 
+
+    public static fibonacci(int n){
+        if(n==0)return 0;
+        int a=0;
+        int b=1;
+        
+        for(int i=2;i<n;i++){
+            int c= a+b;
+            int a=b;
+            int b=c
+            }
+        return a+b;    
+    }
+    //  we also have another more direct bottom-up approach , which just calculate every smaller fibonacci numbers and sum two biggest of them up.
 }
