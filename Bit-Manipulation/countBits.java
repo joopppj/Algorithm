faac947ee672fa3f6c1d1774439f41003ac5ff99
Zhousenye Liu <jooppp@live.com>
2016-12-02 23:34:26 -0500

    Create countBits.java

---
+++ Bit-Manipulation/countBits.java
@@ -0,0 +1,7 @@
+public class countBits {
+    public int[] countBits(int num) {
+        int[] f = new int[num + 1];
+        for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1);
+        return f;
+    }
+}
