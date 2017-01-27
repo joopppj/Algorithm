d2a549309c553ea3692ab6e07cd47792d9ebb1ef
jooppp <jooppp@live.com>
2017-01-27 00:02:51 -0500

    add explanation

--- String/OneEdit.java
+++ String/OneEdit.java
@@ -3,7 +3,7 @@ public class OneEdit{
 	//suppose we only have three operation on a string: removing a character ,replace a character and insert a character
 	// this function returns true , when s1 only need one(or 0) operation to become s2.
 	static boolean OneEdit(String s1,String s2){
-		if(Math.abs(s1.length()-s2.length())>1){
+		if(Math.abs(s1.length()-s2.length())>1){        // check length 
 			return false;
 		}
 		String x=s1.length()<s2.length()?s1:s2;
@@ -11,11 +11,11 @@ public class OneEdit{
 		int i1=0;int i2=0;
 		boolean difference=false;
 		while(i2<y.length()&&i1<x.length()){
-			if(x.charAt(i1)!=y.charAt(i2)){
-				if(difference) return false;
+			if(x.charAt(i1)!=y.charAt(i2)){         
+				if(difference) return false;         // if the current two chars are not equal again  it means it is impossible to make only one operation.
 				difference=true;
 				
-				if(x.length()==y.length()){
+				if(x.length()==y.length()){            // if two Strings' lengths are equal and the current characters are not equal , move the pointer of longer string by one 
 					i1++;
 				}
 			}else{
@@ -23,7 +23,7 @@ public class OneEdit{
 			}
 			i2++;
 		}
-		return true;
+		return true;         
 		
 	}
 }
