ebbed7104ff57cca7bfdac417c0816340cab3698
jooppp <jooppp@live.com>
2017-02-15 23:34:47 -0500

    Create MergeTwoSortedArrays.java

---
+++ Array/MergeTwoSortedArrays.java
@@ -0,0 +1,18 @@
+public class MergeTwoSortedArrays {
+    // merge two sorted arrays .  Assume nums1 has enough space to hold all elements 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j =n-1;
+        int k=n+m-1;
+        while (i>=0&&j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k--]=nums1[i--];
+            }
+            else {
+                nums1[k--]=nums2[j--];
+            }
+        }
+        while(j>=0)nums1[k--]=nums2[j--];
+    }
+    //simple two pointers method. 
+}
