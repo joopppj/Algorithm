d3f062de4ad36a8ae760aceae52b87f5416a9b03
jooppp <jooppp@live.com>
2017-03-09 21:32:58 -0500

    Update IntegerBreak.java

--- Math/IntegerBreak.java
+++ Math/IntegerBreak.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class IntegerBreak {
 //Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
 
 For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
