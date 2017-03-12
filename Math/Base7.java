e79e969320eaa2952cde4d1f95d7736ac83dd8be
jooppp <jooppp@live.com>
2017-03-11 21:01:55 -0500

    Update Base7.java

--- Math/Base7.java
+++ Math/Base7.java
@@ -1,5 +1,5 @@
 public class Base7 {
-     // simple math + recursion problem, get vase 7 of a number 
+     // simple math + recursion problem, get vase 7 of a number , same technique can also answer base 2 base 3.....
     public String convertToBase7(int num) {
         if(num<0){
             return "-"+convertToBase7(-num);
