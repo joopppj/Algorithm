4b546e8f593c8bae013d83a22df0333813c1cce3
jooppp <jooppp@live.com>
2017-03-17 21:50:48 -0400

    Create findDuplicate.java

---
+++ Array/findDuplicate.java
@@ -0,0 +1,19 @@
+public class Solution {
+// q:Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        int slow = n;
+        int fast = n;
+        do{
+            slow = nums[slow-1];
+            fast = nums[nums[fast-1]-1];
+        }while(slow != fast);
+        slow = n;
+        while(slow != fast){
+            slow = nums[slow-1];
+            fast = nums[fast-1];
+        }
+        return slow;
+    }
+    // because there is a duplicate number , so , there will be a cycle in this while loop.  the fast-slow technique can be used here. in the second while loop the fast and slow loop will meet at entry of the cycle
+}
