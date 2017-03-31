247dafe024f8c25b40ad393e8c4c08d1efeb82bf
jooppp <jooppp@live.com>
2017-03-30 23:58:48 -0400

    Update TargetSum.java

--- Array/TargetSum.java
+++ Array/TargetSum.java
@@ -1,9 +1,12 @@
 public class Solution {
+    //You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+Find out how many ways to assign symbols to make sum of integers equal to target S.
     public int findTargetSumWays(int[] nums, int s) {
         int sum = 0;
         for (int n : nums)
             sum += n;
-        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1); 
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1); //if sum of nums plus target is not even or sum of nums is smaller than target , then it is impossible to get subarray
     }
     private int subsetSum(int[] nums, int S){
         int[] dp = new int[S + 1];
