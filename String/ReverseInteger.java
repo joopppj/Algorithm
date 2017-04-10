cefe2f51b2639b3f7d0f3f882045a64f08f2f8e2
jooppp <jooppp@live.com>
2017-04-09 23:49:37 -0400

    Create ReverseInteger.java

---
+++ String/ReverseInteger.java
@@ -0,0 +1,22 @@
+public classReverseInteger {
+    public int reverse(int x) {
+        if(x>=0){
+        String a = x + "";
+        StringBuilder A = new StringBuilder(a);
+        A.reverse();
+        a=A.toString();
+        if (Long.parseLong(a)>Integer.MAX_VALUE) return 0;
+        return Integer.parseInt(a);
+        }
+        else {
+        String a = x + "";
+        a=a.substring(1);
+        StringBuilder A = new StringBuilder(a);
+        A.reverse();
+        a=A.toString();
+        if (Long.parseLong(a)>Integer.MAX_VALUE) return 0;
+        int z= Integer.parseInt(a);
+        return 0-z;
+        }
+    }
+}
