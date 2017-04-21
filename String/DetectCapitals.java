a349d796b17c6175a89c43c8bc0e88615fa76802
jooppp <jooppp@live.com>
2017-04-20 21:53:33 -0400

    Create DetectCapitals.java

---
+++ String/DetectCapitals.java
@@ -0,0 +1,9 @@
+public class DetectCapitals {
+   // q:  determine if a word use capital letters correctly 
+    public boolean detectCapitalUse(String word) {
+         return word.equals(word.toUpperCase()) || 
+               word.equals(word.toLowerCase()) ||
+               Character.isUpperCase(word.charAt(0)) && 
+               word.substring(1).equals(word.substring(1).toLowerCase());
+    }
+}
