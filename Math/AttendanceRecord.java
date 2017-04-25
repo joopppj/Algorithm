8c3d0509b7f5a55bd27f158451cd65d9090bc8cc
jooppp <jooppp@live.com>
2017-04-24 23:46:11 -0400

    Update AttendanceRecord.java

--- Math/AttendanceRecord.java
+++ Math/AttendanceRecord.java
@@ -3,17 +3,6 @@ public class AttendanceRecord {
 
 //You need to return whether the student could be rewarded according to his attendance record.
     public boolean checkRecord(String s) {
-        int L=0;
-        int A=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='L'){
-                L++;
-            }
-            else if(s.charAt(i)=='A'){
-                A++;
-            }
-        }
-        if(L<=2||A<=1)return true;
-        return false;
+        return !s.contains("LLL") && (s.indexOf("A") == s.lastIndexOf("A"));
     }
 }
