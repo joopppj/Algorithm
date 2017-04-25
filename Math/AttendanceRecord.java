4ca5dc7d95ab886944f8a78b897676ba64eb1ade
jooppp <jooppp@live.com>
2017-04-24 23:45:07 -0400

    Update AttendanceRecord.java

--- Math/AttendanceRecord.java
+++ Math/AttendanceRecord.java
@@ -1,4 +1,7 @@
 public class AttendanceRecord {
+    // given a string that includes ALP which represents attend , late , participate . A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+//You need to return whether the student could be rewarded according to his attendance record.
     public boolean checkRecord(String s) {
         int L=0;
         int A=0;
